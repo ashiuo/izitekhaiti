66b4f847a0cbe9101de6c6ae474bbd0b7b3be8dc
Belizaire Kemy Fed <akemyfed@gmail.com>
2025-07-05 11:33:04 -0400

    Create theme.js
    PERSISTENT DARK MODE

---
+++ js/theme.js
@@ -0,0 +1,5 @@
+document.getElementById('toggle-dark').addEventListener('click', function () {
+  document.body.classList.toggle('dark-mode');
+  const isDark = document.body.classList.contains('dark-mode');
+  localStorage.setItem('theme', isDark ? 'dark' : 'light');
+});
